4cef75050b11d379475270bf117136eb49dccfba
Atsuhiro Teshima <hiro@lingualbox.com>
2018-12-18 19:16:36 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; // skip it 
+    e.target.classList.remove('playing');
+  }
+
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return;//stop function
+
+  key.classList.add('playing');
+  audio.currentTime = 0; // rewind to start
+  audio.play();    
+}
+  
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown',playSound);
